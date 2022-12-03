2931f737c5d8c6df7feca0e649ba2d3800cb66fe
Michael Joseph <josephmje.22@gmail.com>
2022-12-03 13:09:28 -0500

    update admin views

--- afp/accounts/forms.py
+++ afp/accounts/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import get_user_model
 
-from .models import CustomUser
-
+User = get_user_model()
 
 class ProfileUpdateForm(UserChangeForm):
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={"class": "form-control"})
     )
 
-    class Meta:
-        model = CustomUser
+    class Meta(UserChangeForm.Meta):
+        model = User
         fields = [
             "first_name",
             "middle_name",
